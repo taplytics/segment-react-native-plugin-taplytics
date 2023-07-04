9e23f68a600898881771dff20b9faaac7187d10b
Chantal Chung <chantal.chung@devcycle.com>
2023-07-04 08:36:30 -0700

    fix: support customData attribute to be passed in

--- src/methods/identify.ts
+++ src/methods/identify.ts
@@ -2,7 +2,7 @@ import type { IdentifyEventType } from '@segment/analytics-react-native';
 import * as Taplytics from 'taplytics-react-native';
 
 export default (event: IdentifyEventType) => {
-  const { email, firstName, lastName, name, age, gender, ...customAttributes } = event.traits;
+  const { email, firstName, lastName, name, age, gender, customData, ...customAttributes } = event.traits;
 
   const userAttributes = {
     user_id: event.userId,
@@ -12,7 +12,10 @@ export default (event: IdentifyEventType) => {
     name,
     age,
     gender,
-    customData: customAttributes,
+    customData: {
+      ...(typeof customData === 'object' ? customData : {}),
+      ...customAttributes,
+    },
   };
 
   const cleanedUserAttributes = JSON.parse(JSON.stringify(userAttributes));
